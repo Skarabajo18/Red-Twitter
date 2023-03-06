b8f48c49d2489cd9909a79d685e58fa0efaa6218
Skarabajo18 <felipesegovia14@gmail.com>
2023-03-06 02:53:03 -0300

    geo

--- social_project/settings.py
+++ social_project/settings.py
@@ -123,6 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (
+    '/static/',
+)
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static/'),
+)
 
 
 LOGIN_REDIRECT_URL = 'home'
